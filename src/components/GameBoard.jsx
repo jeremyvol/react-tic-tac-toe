4021f1c632798879f42fb4f08bc616caf48a5603
Jeremy Vollant <j.vollant@gmail.com>
2025-05-23 20:26:26 +0200

    activePlayer

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,17 +6,18 @@ const initalGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initalGameBoard);
 
-  function handleSelectSquare(rowIndex, colIndex, playerSymbol) {
+  function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
       const updatedBoard = [
         ...prevGameBoard.map((innerArray) => [...innerArray]),
       ];
-      updatedBoard[rowIndex][colIndex] = "X";
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
+    onSelectSquare();
   }
 
   return (
