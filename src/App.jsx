8fb7c8a6eef2ed919aa377b7162b3fb8aec105ca
Jeremy Vollant <j.vollant@gmail.com>
2025-05-26 22:08:29 +0200

    log

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,48 @@
-import Player from "./components/Player";
-import GameBoard from "./components/GameBoard";
-import { useState } from "react";
+import GameBoard from './components/GameBoard';
+import Log from './components/Log';
+import Player from './components/Player';
+import { useState } from 'react';
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
-    setGameTurns();
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer(curActivePlayer => (curActivePlayer === 'X' ? 'O' : 'X'));
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
     <main>
-      <div id="game-container">
-        <ol id="players" className="highlight-player">
+      <div id='game-container'>
+        <ol id='players' className='highlight-player'>
           <Player
-            initialName="Player 1"
-            symbol="X"
-            isActive={activePlayer === "X"}
+            initialName='Player 1'
+            symbol='X'
+            isActive={activePlayer === 'X'}
           />
           <Player
-            initialName="Player 2"
-            symbol="O"
-            isActive={activePlayer === "O"}
+            initialName='Player 2'
+            symbol='O'
+            isActive={activePlayer === 'O'}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      LOG
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,33 +1,41 @@
-import { useState } from "react";
-
 const initalGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initalGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initalGameBoard;
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
-    onSelectSquare();
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
   }
 
+  // const [gameBoard, setGameBoard] = useState(initalGameBoard);
+
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
+  //   onSelectSquare();
+  // }
+
   return (
-    <ol id="game-board">
+    <ol id='game-board'>
       {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button
+                  // () => handleSelectSquare(rowIndex, colIndex)
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
